419fe4dcd93d1d831be3be204994553659d25964
Wesley Moura <54549347+mouraiv@users.noreply.github.com>
2022-09-22 15:40:29 -0300

    Update CSS e JS Datepicker

--- GerenciamentoMercadoria/wwwroot/js/site.js
+++ GerenciamentoMercadoria/wwwroot/js/site.js
@@ -1,24 +1,30 @@
-﻿$('.close-alert').click(function () {
-    $('.alert').hide('hide');
+﻿$(document).ready(function () {
+
+    $('.close-alert').click(function () {
+        $('.alert').hide('hide');
+    });
+
+    $("#datepicker").datepicker({
+        monthNamesShort: ["Janeiro", "Fevereiro", "Março", "Abril", "Março", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: false,
+        dateFormat: 'MM yy',
+        language: "pt-BR",
+        onChangeMonthYear: function (year, month) {
+            $.ajax({
+                url: "/Entrada",
+                type: "POST",
+                data: { seachData: '1/' + month + '/' + year },
+                dataType: "html"
+            }).done(function (resposta) {
+                $("#listaEntrada").html(resposta);
+            });
+        }
+    });
 });
 
-$("#datepicker").datepicker({
-    changeMonth: true,
-    changeYear: true,
-    showButtonPanel: false,
-    dateFormat: 'MM yy',
-    onChangeMonthYear: function (year, month) {
-        $.ajax({
-            url: "/Entrada",
-            type: "POST",
-            data: { seachData: '1/' + month + '/' + year },
-            dataType: "html"
 
-        }).done(function (resposta) {
-            $("#listaEntrada").html(resposta);
-        });
-    }
- });
 
 
 
